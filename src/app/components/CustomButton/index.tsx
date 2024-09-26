d1df1e9e669ac74089a223e637cf854bdf80f24e
sergipastor <sergi@nennisiwok.com>
2024-09-26 17:33:35 +0200

    submitOrder first draft

--- src/app/components/CustomButton/index.tsx
+++ src/app/components/CustomButton/index.tsx
@@ -1,4 +1,35 @@
-import { useAppSelector } from '@/hooks';
+import { useAppSelector } from "@/hooks";
+import { getRdtOrThrow } from "../../subscriptions";
+
+export async function submitOrder() {
+  const rdt = getRdtOrThrow();
+  const account_address =
+    "account_tdx_2_12yas0232myy5vv60hepjznf4m3khjlpyuu7v2ckvtd6337razwwcru";
+  const manifest = `CALL_METHOD
+      Address("component_tdx_2_1cqg6h0xmp6p3h529mq8fnr9m0lvlaaqaam7eec7w3ly7hlmscn675z")
+      "add_position"
+      Address("${account_address}")
+      "long"
+      Decimal("3")
+      Decimal("0.5")
+      Decimal("1")
+  ;
+  CALL_METHOD
+      Address("${account_address}")
+      "try_deposit_batch_or_refund"
+      Expression("ENTIRE_WORKTOP")
+      Enum<0u8>()
+  ;`;
+
+  console.log(manifest);
+  // Send manifest to extension for signing
+  const result = await rdt.walletApi.sendTransaction({
+    transactionManifest: manifest,
+    version: 2,
+  });
+  if (result.isErr()) throw result.error;
+  console.log("Transaction Output: ", result.value);
+}
 
 export default function SubmitButton() {
   const { type } = useAppSelector((state: { perp: any }) => state.perp);
@@ -8,21 +39,23 @@ export default function SubmitButton() {
   return (
     <button
       className={`w-full h-[40px] p-3 my-6 rounded ${
-        type === 'LONG'
-          ? 'bg-radix-green text-black opacity-100'
-          : 'bg-radix-red text-white opacity-100'
+        type === "LONG"
+          ? "bg-radix-green text-black opacity-100"
+          : "bg-radix-red text-white opacity-100"
       }`}
-      onClick={async e => {
+      onClick={async (e) => {
         if (!isConnected) {
-          alert('connect_wallet_to_trade');
+          alert("connect_wallet_to_trade");
           return;
         }
 
         e.stopPropagation();
-        console.log('submit')
+        console.log("submit");
+        submitOrder();
+
         // DToads.promise(
         //   async () => {
-        //     // const action = await dispatch(submitOrder());
+        //     const action = await dispatch(submitOrder());
         //     // if (!action.type.endsWith('fulfilled')) {
         //     //   // Transaction was not fulfilled (e.g. userRejected or userCanceled)
         //     //   throw new Error('Transaction failed due to user action.');
@@ -33,9 +66,9 @@ export default function SubmitButton() {
         //     // dispatch(orderInputSlice.actions.resetUserInput());
         //     // dispatch(fetchBalances());
         //   },
-        //   'submitting order', // Loading message
-        //   'order submitted', // success message
-        //   'failed to submit order' // error message
+        //   "submitting order", // Loading message
+        //   "order submitted", // success message
+        //   "failed to submit order" // error message
         // );
       }}
     >
